6bec2df80e2b558cb23edc193aca808bc317b89a
Gitte Beckmann <gittebeckmann@hotmail.com>
2024-09-11 13:04:38 +0200

    -added another shorter way

--- src/index.js
+++ src/index.js
@@ -1,24 +1,35 @@
-console.log("Start Here 😉");
+// console.log("Start Here 😉");
 
-const darkSwitch = document.getElementById('dark')
-const lightSwitch = document.getElementById('light')
-const crazySwitch = document.getElementById('crazy')
+// const darkSwitch = document.getElementById('dark')
+// const lightSwitch = document.getElementById('light')
+// const crazySwitch = document.getElementById('crazy')
 
-const box = document.getElementById('box')
+// const box = document.getElementById('box')
 
-darkSwitch.addEventListener("click", () => {
-    box.classList.add("dark")
-    box.classList.remove("light")
-    box.classList.remove("crazy")
-})
+// darkSwitch.addEventListener("click", () => {
+//     box.classList.add("dark")
+//     box.classList.remove("light")
+//     box.classList.remove("crazy")
+// })
 
-lightSwitch.addEventListener("click", () => {
-    box.classList.add("light")
-    box.classList.remove("dark")
-    box.classList.remove("crazy")
-})
+// lightSwitch.addEventListener("click", () => {
+//     box.classList.add("light")
+//     box.classList.remove("dark")
+//     box.classList.remove("crazy")
+// })
 
-crazySwitch.addEventListener("click", () => {
-    box.classList.add("crazy")
-})
+// crazySwitch.addEventListener("click", () => {
+//     box.classList.add("crazy")
+// })
+
+const changeBox = document.getElementById("box")
+
+const changeTheme = (theme) => {
+    changeBox.classList.remove("dark", "light", "crazy")
+    changeBox.classList.add(theme)
+}
+
+document.getElementById("dark").addEventListener("click", () => changeTheme("dark"))
+document.getElementById("light").addEventListener("click", () => changeTheme("light"))
+document.getElementById("crazy").addEventListener("click", () => changeTheme("crazy"))
 
