222005d38b964f3e117114e567852d80d13f6473
Gitte Beckmann <gittebeckmann@hotmail.com>
2024-09-11 11:01:37 +0200

    - changed background-color through DOM manipulation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 console.log("Start Here 😉");
+
+const darkSwitch = document.getElementById('dark')
+const lightSwitch = document.getElementById('light')
+const crazySwitch = document.getElementById('crazy')
+
+const box = document.getElementById('box')
+
+darkSwitch.addEventListener("click", () => {
+    box.classList.add("dark")
+    box.classList.remove("light")
+    box.classList.remove("crazy")
+})
+
+lightSwitch.addEventListener("click", () => {
+    box.classList.add("light")
+    box.classList.remove("dark")
+    box.classList.remove("crazy")
+})
+
+crazySwitch.addEventListener("click", () => {
+    box.classList.add("crazy")
+})
+
